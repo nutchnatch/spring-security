74c61198863468ef035c0628704f913662481b4f
nivaldomonteiro <nushnivaldo@gmail.com>
2022-02-08 21:50:00 +0000

    Added cors configuration using spring security

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -20,9 +20,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
  * Created by jt on 6/13/20.
  */
 @RequiredArgsConstructor
-/*@Configuration
+@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)*/
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
@@ -40,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 http.addFilterBefore(google2faFilter, SessionManagementFilter.class);
 
-                http
+                http.cors().and()
                 .authorizeRequests(authorize -> {
                     authorize
                             .antMatchers("/h2-console/**").permitAll() //do not use in production!
